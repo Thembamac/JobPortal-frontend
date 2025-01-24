35c0180f9bfd4882ca22aacf79bb48ddfa5a4f4d
Thembamac <thembamacheke.tm@gmail.com>
2025-01-25 01:00:19 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-//import { theme } from './theme';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ProSidebarProvider } from 'react-pro-sidebar';
@@ -22,12 +21,15 @@ import Register from './pages/Register';
 import DashCategory from './pages/admin/DashCategory';
 import DashCreateJob from './pages/admin/DashCreateJob';
 import DashCreateCategory from './pages/admin/DashCreateCategory';
-
-
 import { createTheme } from '@mui/material/styles';
-import { themeColors } from './theme'
+import { themeColors } from './theme';
 import { useSelector } from 'react-redux';
 import { useMemo } from 'react';
+import axios from 'axios';
+
+// Configure axios defaults
+axios.defaults.baseURL = 'https://mernbackend-91l7.onrender.com/';
+axios.defaults.withCredentials = true;
 
 //HOC
 const UserDashboardHOC = Layout(UserDashboard);
@@ -36,48 +38,44 @@ const UserInfoDashboardHOC = Layout(UserInfoDashboard);
 const AdminDashboardHOC = Layout(AdminDashboard);
 const DashUsersHOC = Layout(DashUsers);
 const DashJobsHOC = Layout(DashJobs);
-const DashCategoryHOC = Layout(DashCategory)
-const DashCreateJobHOC = Layout(DashCreateJob)
-const DashCreateCategoryHOC = Layout(DashCreateCategory)
-
-
-
-
+const DashCategoryHOC = Layout(DashCategory);
+const DashCreateJobHOC = Layout(DashCreateJob);
+const DashCreateCategoryHOC = Layout(DashCreateCategory);
 
 const App = () => {
-    const { mode } = useSelector((state) => state.mode);
-    const theme = useMemo(() => createTheme(themeColors(mode)), [mode]);
+   const { mode } = useSelector((state) => state.mode);
+   const theme = useMemo(() => createTheme(themeColors(mode)), [mode]);
 
-    return (
-        <>
-            <ToastContainer />
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <ProSidebarProvider>
-                    <BrowserRouter>
-                        <Routes>
-                            <Route path='/' element={<Home />} />
-                            <Route path='/search/location/:location' element={<Home />} />
-                            <Route path='/search/:keyword' element={<Home />} />
-                            <Route path='/login' element={<LogIn />} />
-                            <Route path='/register' element={<Register />} />
-                            <Route path='/job/:id' element={<SingleJob />} />
-                            <Route path='/admin/dashboard' element={<AdminRoute><AdminDashboardHOC /></AdminRoute>} />
-                            <Route path='/admin/users' element={<AdminRoute><DashUsersHOC /></AdminRoute>} />
-                            <Route path='/admin/jobs' element={<AdminRoute><DashJobsHOC /></AdminRoute>} />
-                            <Route path='/admin/category' element={<AdminRoute><DashCategoryHOC /></AdminRoute>} />
-                            <Route path='/admin/job/create' element={<AdminRoute><DashCreateJobHOC /></AdminRoute>} />
-                            <Route path='/admin/category/create' element={<AdminRoute><DashCreateCategoryHOC /></AdminRoute>} />
-                            <Route path='/user/dashboard' element={<UserRoute>< UserDashboardHOC /></UserRoute>} />
-                            <Route path='/user/jobs' element={<UserRoute>< UserJobsHistoryHOC /></UserRoute>} />
-                            <Route path='/user/info' element={<UserRoute>< UserInfoDashboardHOC /></UserRoute>} />
-                            <Route path='*' element={<NotFound />} />
-                        </Routes>
-                    </BrowserRouter>
-                </ProSidebarProvider>
-            </ThemeProvider>
-        </>
-    )
+   return (
+       <>
+           <ToastContainer />
+           <ThemeProvider theme={theme}>
+               <CssBaseline />
+               <ProSidebarProvider>
+                   <BrowserRouter>
+                       <Routes>
+                           <Route path='/' element={<Home />} />
+                           <Route path='/search/location/:location' element={<Home />} />
+                           <Route path='/search/:keyword' element={<Home />} />
+                           <Route path='/login' element={<LogIn />} />
+                           <Route path='/register' element={<Register />} />
+                           <Route path='/job/:id' element={<SingleJob />} />
+                           <Route path='/admin/dashboard' element={<AdminRoute><AdminDashboardHOC /></AdminRoute>} />
+                           <Route path='/admin/users' element={<AdminRoute><DashUsersHOC /></AdminRoute>} />
+                           <Route path='/admin/jobs' element={<AdminRoute><DashJobsHOC /></AdminRoute>} />
+                           <Route path='/admin/category' element={<AdminRoute><DashCategoryHOC /></AdminRoute>} />
+                           <Route path='/admin/job/create' element={<AdminRoute><DashCreateJobHOC /></AdminRoute>} />
+                           <Route path='/admin/category/create' element={<AdminRoute><DashCreateCategoryHOC /></AdminRoute>} />
+                           <Route path='/user/dashboard' element={<UserRoute>< UserDashboardHOC /></UserRoute>} />
+                           <Route path='/user/jobs' element={<UserRoute>< UserJobsHistoryHOC /></UserRoute>} />
+                           <Route path='/user/info' element={<UserRoute>< UserInfoDashboardHOC /></UserRoute>} />
+                           <Route path='*' element={<NotFound />} />
+                       </Routes>
+                   </BrowserRouter>
+               </ProSidebarProvider>
+           </ThemeProvider>
+       </>
+   )
 }
 
 export default App
