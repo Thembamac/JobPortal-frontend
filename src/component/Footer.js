8eadb122387225c47b581774b0dd74c029b75741
Thembamac <thembamacheke.tm@gmail.com>
2025-01-25 12:45:26 +0200

    Update footer again

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,28 +6,20 @@ const Footer = () => {
    const { palette } = useTheme();
    return (
        <Box sx={{
-           height: '40px',
+           height: '60px',
            bgcolor: palette.secondary.midNightBlue,
            display: 'flex',
            justifyContent: 'center',
            alignItems: 'center',
-           position: 'fixed', 
            bottom: 0,
            left: 0,
            right: 0,
            width: '100%',
            zIndex: 1000
        }}>
-           <Typography 
-               variant="body2" 
-               sx={{ 
-                   color: palette.primary.main,
-                   fontSize: '12px',
-                   fontWeight: 700
-               }}
-           >
+           <Box component='span' sx={{ color: palette.primary.main }}>
                © 2025 G1 Vision. All rights reserved
-           </Typography>
+           </Box>
        </Box>
    )
 }
